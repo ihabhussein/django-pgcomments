242069f15d770eaad7a7369ba01095a37f041620
Ihab Hussein <ihab@ihabhussein.com>
2018-05-26 20:06:33 +0200

    Add view to create posts in demo project

--- demo/blog/views.py
+++ demo/blog/views.py
@@ -1,17 +1,19 @@
 from django.http import HttpResponseRedirect
 from django.urls import reverse
 from django.shortcuts import get_object_or_404
-from django.views.generic import View
 from .models import Post
 
 
-class AddCommentView(View):
-    def post(self, request, *args, **kwargs):
-        pk = kwargs.get('pk')
-        path = request.POST.get('path', '')
-        text = request.POST.get('text', '')
+def addPost(request):
+    Post().save()
+    return HttpResponseRedirect(reverse('index'))
 
-        post = get_object_or_404(Post, pk=pk)
-        post.comments.add_comment(path, request.user, text)
+def addComment(request, *args, **kwargs):
+    pk = kwargs.get('pk')
+    path = request.POST.get('path', '')
+    text = request.POST.get('text', '')
 
-        return HttpResponseRedirect(reverse('post', kwargs={'pk': pk}))
+    post = get_object_or_404(Post, pk=pk)
+    post.comments.add_comment(path, request.user, text)
+
+    return HttpResponseRedirect(reverse('post', kwargs={'pk': pk}))
--- demo/website/urls.py
+++ demo/website/urls.py
@@ -2,11 +2,12 @@ from django.urls import path, include
 from django.contrib import admin
 from django.views.generic import ListView, DetailView, View
 from demo.blog.models import Post
-from demo.blog.views import AddCommentView
+from demo.blog.views import addPost, addComment
 
 
 urlpatterns = [
     path('', ListView.as_view(model=Post, ordering='-pk'), name='index'),
+    path('add-post', addPost, name='add_post'),
     path('<int:pk>/', DetailView.as_view(model=Post), name='post'),
-    path('<int:pk>/add-comment', AddCommentView.as_view(), name='add_comment'),
+    path('<int:pk>/add-comment', addComment, name='add_comment'),
 ]
