acfeb3d41f5193d0ca210e433d093b692e4d1e37
Ihab Hussein <ihab@ihabhussein.com>
2018-05-27 14:20:26 +0200

    code reformat

--- pgcomments/models.py
+++ pgcomments/models.py
@@ -26,9 +26,10 @@ class Thread(models.Model):
 
     def add_comment(self, path, user, text):
         with connection.cursor() as cursor:
-            cursor.execute("SELECT pgcomments_add_comment(%s, %s, %s, %s)", (
-                self.pk, self._fix_path(path), user.username, text,
-            ))
+            cursor.execute(
+                "SELECT pgcomments_add_comment(%s, %s, %s, %s)",
+                (self.pk, self._fix_path(path), user.username, text)
+            )
 
     def set_attribute(self, path, name, value):
         if name in RESERVED_KEYS:
@@ -37,14 +38,16 @@ class Thread(models.Model):
             """.format(RESERVED_KEYS))
 
         with connection.cursor() as cursor:
-            cursor.execute("SELECT pgcomments_set_attribute(%s, %s, %s, %s::jsonb)", (
-                self.pk, self._fix_path(path), name, json.dumps(value),
-            ))
+            cursor.execute(
+                "SELECT pgcomments_set_attribute(%s, %s, %s, %s::jsonb)",
+                (self.pk, self._fix_path(path), name, json.dumps(value))
+            )
 
     def get_attribute(self, path, name):
         with connection.cursor() as cursor:
-            cursor.execute("SELECT pgcomments_get_attribute(%s, %s, %s)", (
-                self.pk, self._fix_path(path), name,
-            ))
+            cursor.execute(
+                "SELECT pgcomments_get_attribute(%s, %s, %s)",
+                (self.pk, self._fix_path(path), name)
+            )
             row = cursor.fetch_one()
             return json.loads(row[0])
