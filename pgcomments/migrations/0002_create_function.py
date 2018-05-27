5986afa5e03595d8fad20e12e9441107d8964cc7
Ihab Hussein <ihab@ihabhussein.com>
2018-05-27 14:00:04 +0200

    Now with attributes (get/set)

--- pgcomments/migrations/0002_create_function.py
+++ pgcomments/migrations/0002_create_function.py
@@ -39,18 +39,35 @@ class Migration(migrations.Migration):
         """),
 
         migrations.RunSQL("""
-            CREATE OR REPLACE FUNCTION pgcomments_set_comment_attributes (
+            CREATE OR REPLACE FUNCTION pgcomments_set_attribute (
                 _id integer,
                 _path integer[],
-                _new_value jsonb
+                _name text,
+                _value jsonb
             ) RETURNS VOID LANGUAGE SQL AS $$
                 WITH X AS (
                     SELECT ('{' || array_to_string(_path, ',children,') || '}')::text[] AS path,
-                           _new_value::jsonb AS new_value
+                           jsonb_build_object(_name, _value) AS new_value
                 )
                 UPDATE pgcomments_thread
                    SET thread = jsonb_set(thread, X.path, thread#>X.path || X.new_value)
                   FROM X
+                 WHERE id = _id;
+            $$;
+        """, reverse_sql="""
+            DROP FUNCTION IF EXISTS pgcomments_set_comment_attributes (integer, integer[], jsonb);
+        """),
+
+        migrations.RunSQL("""
+            CREATE OR REPLACE FUNCTION pgcomments_get_attribute (
+                _id integer,
+                _path integer[],
+                _name text
+            ) RETURNS jsonb LANGUAGE SQL AS $$
+                WITH X AS (
+                    SELECT ('{' || array_to_string(_path, ',children,') || ',' || _name || '}')::text[] AS path
+                )
+                SELECT thread#>X.path FROM pgcomments_thread, X
                  WHERE id = _id;
             $$;
         """, reverse_sql="""
--- pgcomments/models.py
+++ pgcomments/models.py
@@ -21,12 +21,20 @@ class Thread(models.Model):
 
     def add_comment(self, path, user, text):
         with connection.cursor() as cursor:
-            cursor.execute("SELECT pgcomments_add_comment(%s, %s, %s, %s);", (
+            cursor.execute("SELECT pgcomments_add_comment(%s, %s, %s, %s)", (
                 self.pk, self._fix_path(path), user.username, text,
             ))
 
-    def set_comment_attributes(self, path, new_value):
+    def set_attribute(self, path, name, value):
         with connection.cursor() as cursor:
-            cursor.execute("SELECT pgcomments_set_comment_attributes(%s, %s, %s);", (
-                self.pk, self._fix_path(path), json.dumps(new_value),
+            cursor.execute("SELECT pgcomments_set_attribute(%s, %s, %s, %s::jsonb)", (
+                self.pk, self._fix_path(path), name, json.dumps(value),
             ))
+
+    def get_attribute(self, path, name):
+        with connection.cursor() as cursor:
+            cursor.execute("SELECT pgcomments_get_attribute(%s, %s, %s)", (
+                self.pk, self._fix_path(path), name,
+            ))
+            row = cursor.fetch_one()
+            return json.loads(row[0])
